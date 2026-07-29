ea1d149f5e1d3b8a30830bafe0a0ec0dc8ecc290
nascimento-jean <110572557+nascimento-jean@users.noreply.github.com>
2026-07-29 11:51:20 -0300

    Adiciona service-worker.js ao aplicativo

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE="despesas-pessoais-github-v3";
+const ROOT="/despesas-pessoais/";
+const SHELL=[ROOT,ROOT+"index.html",ROOT+"manifest.webmanifest",ROOT+"icon.svg"];
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting()});
+self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  if(event.request.mode==="navigate"){
+    event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(ROOT+"index.html")));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response})));
+});
+
